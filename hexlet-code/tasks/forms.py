196301a3660286ffdf685c15cebaf7a735ac368b
mishablokhin <mishablokhin@yandex.ru>
2025-04-10 16:18:14 +0300

    Fixed linter errors

--- hexlet-code/tasks/forms.py
+++ hexlet-code/tasks/forms.py
@@ -14,4 +14,4 @@ class TaskForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['executor'].label_from_instance = \
             lambda obj: f"{obj.first_name} {obj.last_name}".strip() \
-                        or obj.username
+            or obj.username
