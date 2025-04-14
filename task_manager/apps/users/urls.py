015813e2e073b652460bcf4b68615a8c69d969a4
mishablokhin <mishablokhin@yandex.ru>
2025-04-14 15:20:30 +0300

    Fixed user creation url from register to create

--- task_manager/apps/users/urls.py
+++ task_manager/apps/users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.UserListView.as_view(), name='user_list'),
-    path('register/', views.UserCreateView.as_view(), name='register'),
+    path('create/', views.UserCreateView.as_view(), name='register'),
     path('<int:pk>/update/', views.UserUpdateView.as_view(),
          name='user_update'),
     path('<int:pk>/delete/', views.UserDeleteView.as_view(),
